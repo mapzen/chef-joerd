db6e9a6affa37d3073c991d36912408e8a5f0f9f
Matt Amos <matt.amos@mapzen.com>
2016-08-24 14:04:53 +0100

    Add environment variable to avoid GDAL bug.

--- recipes/processor.rb
+++ recipes/processor.rb
@@ -14,4 +14,7 @@ runit_service 'joerd' do
   log             true
   default_logger  true
   sv_timeout      node[:joerd][:runit][:timeout]
+  # set this environment variable so that GDAL doesn't try to intepret some
+  # SRTM HGT files as JPEGs. See https://trac.osgeo.org/gdal/ticket/3305
+  env             { "GDAL_SKIP" => "JPEG" }
 end
